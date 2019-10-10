bcd9aabe7dcfdaefe202efd80457d26943ff3af8
Zhen Yang (zyang2) <zyang2@cisco.com>
2019-10-10 23:31:13 +0800

    add jobCommonInit

---
+++ vars/jobCommonInit.groovy
@@ -0,0 +1,51 @@
+
+import com.cloudbees.groovy.cps.NonCPS
+import jenkins.model.ArtifactManager
+import jenkins.util.VirtualFile
+import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
+//import org.jenkinsci.plugins.ghprb.GhprbTrigger
+import java.nio.file.StandardCopyOption
+import java.util.regex.Matcher
+import java.nio.file.Files
+import java.util.regex.Pattern
+
+
+def call(Map parameters = [:], Closure body){
+    def job_params=parameters.get("job_params", [])
+    def block_build = [
+        $class: 'BuildBlockerProperty',
+        useBuildBlocker: true,
+        blockLevel: "NODE",
+        scanQueueFor: "DISABLED",
+        blockingJobs: ".*",
+    ]
+
+    def property_ls = [
+        block_build,
+        buildDiscarder(
+            logRotator(
+                daysToKeepStr: '5',
+                numToKeepStr: '',
+                artifactDaysToKeepStr: '7',
+                artifactNumToKeepStr: '16',
+            )
+        ),
+        [
+            $class: 'EnvInjectJobProperty',
+            info:[
+                propertiesFilePath: '$ATG_job_propfile',
+                loadFilesFromMaster: true,
+            ],
+            on: true,
+            keepJenkinsSystemVariables: true,
+            keepBuildVariables: true,
+            overrideBuildParameters: false,
+
+        ],
+        parameters(
+            job_params
+        ),
+    ]
+
+    properties(property_ls)
+}
